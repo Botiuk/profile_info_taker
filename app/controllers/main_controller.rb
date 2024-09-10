3b5cb89a755b08ac9774cd8e7f3cb91afd2733ed
botiuk <svetabotiuk@gmail.com>
2024-09-10 14:01:22 +0300

    add main controller, routes, views, locales keys

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class MainController < ApplicationController
+  def index; end
+
+  def search
+    if params[:login].present?
+      browser = Watir::Browser.new headless: true
+      browser.goto("https://github.com/#{params[:login]}?tab=repositories")
+
+      if browser.span(class: 'p-name vcard-fullname d-block overflow-hidden').present?
+        @username = browser.span(class: 'p-name vcard-fullname d-block overflow-hidden').text
+        @repositories = []
+        if browser.ul(data_filterable_for: 'your-repos-filter').present?
+          loop do
+            repositories = browser.divs(class: 'd-inline-block mb-1')
+
+            repositories.each do |rep|
+              @repositories << rep.h3.a.text
+            end
+
+            break unless browser.a(class: 'next_page').present?
+
+            browser.a(class: 'next_page').click
+            sleep 2
+          end
+        end
+      end
+
+      browser.close
+      @search_params = params[:login]
+    else
+      redirect_to root_path, alert: t('alert.search')
+    end
+  end
+end
