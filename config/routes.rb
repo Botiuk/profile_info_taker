212c80b50293ea6c26566d2080468b682134124c
botiuk <svetabotiuk@gmail.com>
2024-09-11 10:10:47 +0300

    add set_locale from http_accept_language

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get 'search', to: 'main#search'
-  # Defines the root path route ("/")
-  root 'main#index'
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    get 'search', to: 'main#search'
+
+    root 'main#index'
+  end
 end
